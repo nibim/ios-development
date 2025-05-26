bfcce88be804a96db53402fd38a73f3b89330945
nibim <nibm@kth.se>
2025-05-26 11:48:14 +0200

    Moved flash-chat project into Udemy course folder and added new Secrets.swift

---
+++ Udemy course/ByteCoin/ByteCoin/Secrets.swift
@@ -0,0 +1,12 @@
+//
+//  Secrets.swift
+//  ByteCoin
+//
+//  Created by Nima Beig Mohammadi on 25.05.25.
+//  Copyright © 2025 The App Brewery. All rights reserved.
+//
+
+import Foundation
+struct Secrets {
+    static let coinAPIKey = "97b609a3-32be-47d3-857e-016984aa1eb6"
+}
