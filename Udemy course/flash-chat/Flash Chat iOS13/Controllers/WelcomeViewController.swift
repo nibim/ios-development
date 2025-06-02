7812bfb658c48ee6e7253e1fa6b7c1cb05240775
nibim <nibm@kth.se>
2025-06-02 15:58:55 +0200

    [FlashChat] Firebase Authentication for LogIning

--- Udemy course/flash-chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Udemy course/flash-chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,16 +7,19 @@
 //
 
 import UIKit
-
+import Firebase
 class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if let app = FirebaseApp.app() {
+                print("✅ Firebase is initialized: \(app.name)")
+            } else {
+                print("❌ Firebase is NOT initialized")
+        }
        
     }
-    
 
 }
